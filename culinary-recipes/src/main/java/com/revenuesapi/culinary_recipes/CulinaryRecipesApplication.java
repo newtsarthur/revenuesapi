bc7347f4930e6bc0ba7a7f103caf98dcad990e17
newtsarthur <tutuhoficial13@gmail.com>
2024-12-07 19:36:04 -0300

    correção de bugs

---
+++ culinary-recipes/src/main/java/com/revenuesapi/culinary_recipes/CulinaryRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.revenuesapi.culinary_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulinaryRecipesApplication.class, args);
+	}
+
+}
---
+++ culinary-recipes/src/test/java/com/revenuesapi/culinary_recipes/CulinaryRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revenuesapi.culinary_recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinaryRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
